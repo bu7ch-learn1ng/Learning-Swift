6d27d622858524d603ecc20c66158c70defc9184
Conad Oldcorn <coldcorn@gmail.com>
2015-04-03 16:46:59 +0100

    Operators

--- Operators.playground/section-1.swift
+++ Operators.playground/section-1.swift
@@ -2,7 +2,13 @@
 
 import UIKit
 
-let height : Int = 12 //Feet
-let width : Int = 10 //Feet
+let height = 12 //Feet
+let width = 10 //Feet
 
 let area = height * width
+
+let areaInMetres = Double(area) / 10.764 //converts 'area' into Double
+
+let chairWidth = 3 //feet
+let spaceRemaining = width % chairWidth
+let chairs = width / chairWidth
