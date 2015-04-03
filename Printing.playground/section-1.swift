bc0fb2fafd86acd6ff905d0cf36ed98a05241af6
Conad Oldcorn <coldcorn@gmail.com>
2015-04-03 16:08:26 +0100

    Updates

--- Printing.playground/section-1.swift
+++ Printing.playground/section-1.swift
@@ -2,4 +2,14 @@
 
 import UIKit
 
-var str = "Hello, playground"
+var str : String = "Hello "
+
+let modernProgrammingLanguage : String = "Swift" //Constant
+
+var greeting = str + modernProgrammingLanguage
+
+
+println("To build an iPhone app, we're going to need to learn \(modernProgrammingLanguage)")
+
+//print(modernProgrammingLanguage)
+
