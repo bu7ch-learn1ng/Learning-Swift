775e4b4485569287e3a9f3db951b2b7e98475d74
Conad Oldcorn <coldcorn@gmail.com>
2015-04-03 19:23:36 +0100

    Function argument

--- Functions.playground/section-1.swift
+++ Functions.playground/section-1.swift
@@ -4,11 +4,9 @@ import UIKit
 
 //println("Swift Functions")
 
-func areaCalculation() {
-    let height = 12
-    let width = 10
+func areaCalculation(height: Int, width: Int) {
     let area = height * width
     println("The area of the room is \(area)")
 }
 
-areaCalculation()
+areaCalculation(12,10)
