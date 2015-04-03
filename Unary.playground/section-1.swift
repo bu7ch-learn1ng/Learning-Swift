6b53dfdc64e826ee0567fc2277859271acf1cf2b
Conad Oldcorn <coldcorn@gmail.com>
2015-04-03 17:07:35 +0100

    Unary Operators

---
+++ Unary.playground/section-1.swift
@@ -0,0 +1,23 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var levelScore = 0
+
+levelScore--
+levelScore
+
+// ++ increment
+// -- decrement
+// - negative operator
+
+var totalScore = ++levelScore
+
+levelScore
+totalScore
+
+// ! NOT operator
+
+let on = false
+let off = !on
+
