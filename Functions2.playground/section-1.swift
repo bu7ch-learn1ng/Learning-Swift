18004964b302d97af1ed1390f2a95eab1375880a
Conrad Oldcorn <coldcorn@gmail.com>
2015-04-04 13:21:58 +0100

    optionals fun

--- Functions2.playground/section-1.swift
+++ Functions2.playground/section-1.swift
@@ -37,3 +37,35 @@ let result = searchNames(name: "Conrad")
 
 result.found
 result.description
+
+
+//Optionals - which apt has violated rental agreement + send notice
+
+func findApt (aptNumber : String ) -> String? {
+    let aptNumbers = ["101", "202", "303","404"]
+    for tempAptNumber in aptNumbers {
+        if (tempAptNumber == aptNumber) {
+            return aptNumber
+        }
+    }
+    return nil
+}
+
+
+if let culprit = findApt("502") {
+    println("Apt Found!")
+} else {
+    println("Apartment Not found")
+}
+
+
+
+
+
+
+
+
+
+
+
+
